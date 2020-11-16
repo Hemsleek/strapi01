56b9fcf3e6ed677a2ab08ff0f25aa31c64b52ee7
hemsleek <asiyanbimubashir@gmail.com>
2020-11-16 23:14:04 +0100

    feat: admin dashboard

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'dfe5f4a5966688773406442491c53e80'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '79c70193-0d6d-461f-b7eb-c11a2bab5dfa'
+};
